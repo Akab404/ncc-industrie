e6b7d6a52af7e078b8a39ffb85eb039c55b10648
florian wagrez <florian.wagrez@gmail.com>
2025-06-02 13:49:27 +0200

    fix model contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,16 +4,14 @@ class Contact < MailForm::Base
   attribute :message,   validate: true
   attribute :nickname,  captcha: true
 
-  # Declare the e-mail headers. It accepts anything the mail method
-  # in ActionMailer accepts.
   def headers
     Rails.logger.info "→ Préparation de l'envoi de mail avec MailForm"
     {
-      :subject => "Nouveau message depuis NCC Website",
+      subject: "Nouveau message depuis NCC Website",
       to: ["contact@ncc-industrie.com", "rh@ncc-industrie.com"],
-      :cci => "florian.wagrez@gmail.com",
-      :from => 'contact@ncc-industrie.com',
-      :reply_to => email
+      bcc: "florian.wagrez@gmail.com",
+      from: 'contact@ncc-industrie.com',
+      reply_to: email
     }
   end
 end
